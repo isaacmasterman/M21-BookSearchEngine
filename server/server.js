15ab3c817445a542f9b677c8e32d27491c9afa4a
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2024-02-12 16:48:48 +1100

    Apollo server setup and authetification middleware modification

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +12,34 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// Creating an instance of ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // Apply the auth middleware to every request
+    const auth = authMiddleware({ req });
+    return { auth };
+  },
 });
+
+// Starting Apollo Server and applying it as middleware to the Express app
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
+
+// Initialize Apollo Server
+startApolloServer();
